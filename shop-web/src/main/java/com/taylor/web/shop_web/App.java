8767a16795c9b176d9c55d8e95edd5b92960ec7e
taylor <516195940@qq.com>
2016-04-16 21:53:09 +0800

    代码提交

---
+++ shop-web/src/main/java/com/taylor/web/shop_web/App.java
@@ -0,0 +1,13 @@
+package com.taylor.web.shop_web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
